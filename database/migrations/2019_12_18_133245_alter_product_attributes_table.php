c67d80a46c7a0fa61c0cd21f226c536504dc2c03
Denya86 <denya4444@gmail.com>
2019-12-20 12:04:47 +0200

    Creating Frontend Layout and Partials Creating Login Page Updating Login Controller Creating Registration Page Updating Registration Controller

--- database/migrations/2019_12_18_133245_alter_product_attributes_table.php
+++ database/migrations/2019_12_18_133245_alter_product_attributes_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class AlterProductAttributesTable extends Migration
 {
     /**
@@ -14,14 +12,11 @@ class AlterProductAttributesTable extends Migration
     public function up()
     {
         Schema::table('product_attributes', function (Blueprint $table) {
-
             $table->unsignedBigInteger('attribute_id')->after('id');
             $table->foreign('attribute_id')->references('id')->on('attributes');
-
             $table->string('value')->after('attribute_id');
         });
     }
-
     /**
      * Reverse the migrations.
      *
